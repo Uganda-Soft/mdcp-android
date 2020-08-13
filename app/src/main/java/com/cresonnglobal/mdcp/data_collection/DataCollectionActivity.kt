651c5c329a790a677ad477b1b9980596d7fc63c0
simonojok19 <simonojok19@gmail.com>
2020-08-13 09:54:34 +0300

    added data collection activity view models complete

---
+++ app/src/main/java/com/cresonnglobal/mdcp/data_collection/DataCollectionActivity.kt
@@ -0,0 +1,12 @@
+package com.cresonnglobal.mdcp.data_collection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cresonnglobal.mdcp.R
+
+class DataCollectionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_data_collection)
+    }
+}
---
+++ app/src/main/java/com/cresonnglobal/mdcp/data_collection/DataCollectionActivityViewModel.kt
@@ -0,0 +1,8 @@
+package com.cresonnglobal.mdcp.data_collection
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class DataCollectionActivityViewModel(application: Application) : AndroidViewModel(application) {
+
+}
