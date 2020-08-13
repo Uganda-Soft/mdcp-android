8785f6da302a0e2f5c218877dbe23395ee4d4feb
simonojok19 <simonojok19@gmail.com>
2020-08-13 14:53:00 +0300

    working on the project for simon

--- app/src/main/java/com/cresonnglobal/mdcp/MainActivity.kt
+++ app/src/main/java/com/cresonnglobal/mdcp/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.cresonnglobal.mdcp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.cresonnglobal.mdcp.data_collection.DataCollectionActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button: Button = sample;
+        button.setOnClickListener {
+//            val intent = Intent(MainActivity.class, DataCollectionActivity)
+//            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/cresonnglobal/mdcp/data_collection/DataCollectionActivity.kt
+++ app/src/main/java/com/cresonnglobal/mdcp/data_collection/DataCollectionActivity.kt
@@ -1,12 +1,19 @@
 package com.cresonnglobal.mdcp.data_collection
 
+import android.content.res.AssetManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.cresonnglobal.mdcp.R
+import java.io.InputStream
 
 class DataCollectionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_data_collection)
+
+        val assetManager: AssetManager = assets;
+        val inputStream: InputStream = assetManager.open("question/sample.json")
+        Toast.makeText(this, inputStream.toString(), Toast.LENGTH_SHORT).show();
     }
 }
