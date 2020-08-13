d1e4077ff7d0c15afdaf5b4a1c1a33d29c0f5ca2
simonojok19 <simonojok19@gmail.com>
2020-08-13 09:39:41 +0300

    added video fragment

---
+++ app/src/main/java/com/cresonnglobal/mdcp/widgets/video/VideoFragment.kt
@@ -0,0 +1,32 @@
+package com.cresonnglobal.mdcp.widgets.video
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.cresonnglobal.mdcp.R
+
+class VideoFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = VideoFragment()
+    }
+
+    private lateinit var viewModel: VideoViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.video_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(VideoViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/cresonnglobal/mdcp/widgets/video/VideoViewModel.kt
@@ -0,0 +1,7 @@
+package com.cresonnglobal.mdcp.widgets.video
+
+import androidx.lifecycle.ViewModel
+
+class VideoViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
