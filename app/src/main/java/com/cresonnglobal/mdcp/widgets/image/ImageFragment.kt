eb958fd9fa0564e8515396c640a1c0d13b8be952
simonojok19 <simonojok19@gmail.com>
2020-08-22 13:57:32 +0300

    added image fragment

---
+++ app/src/main/java/com/cresonnglobal/mdcp/widgets/image/ImageFragment.kt
@@ -0,0 +1,32 @@
+package com.cresonnglobal.mdcp.widgets.image
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.cresonnglobal.mdcp.R
+
+class ImageFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ImageFragment()
+    }
+
+    private lateinit var viewModel: ImageViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.image_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(ImageViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/cresonnglobal/mdcp/widgets/image/ImageViewModel.kt
@@ -0,0 +1,7 @@
+package com.cresonnglobal.mdcp.widgets.image
+
+import androidx.lifecycle.ViewModel
+
+class ImageViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
