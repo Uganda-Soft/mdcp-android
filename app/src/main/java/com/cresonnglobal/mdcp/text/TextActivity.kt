d373cf512b252fb3b9c596c857f272734f264f3e
simonojok19 <simonojok19@gmail.com>
2020-08-25 18:28:59 +0300

    added the short text activity

--- app/src/main/java/com/cresonnglobal/mdcp/text/TextActivity.kt
+++ app/src/main/java/com/cresonnglobal/mdcp/text/TextActivity.kt
@@ -2,6 +2,7 @@ package com.cresonnglobal.mdcp.text
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cresonnglobal.mdcp.R
 
 class TextActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
