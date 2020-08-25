561b729bdcff4bc7c7a6505c16f3d6e3e5ecfca5
simonojok19 <simonojok19@gmail.com>
2020-08-25 18:49:31 +0300

    added time picker

---
+++ app/src/main/java/com/cresonnglobal/mdcp/time/TimeActivity.kt
@@ -0,0 +1,12 @@
+package com.cresonnglobal.mdcp.time
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cresonnglobal.mdcp.R
+
+class TimeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_time)
+    }
+}
