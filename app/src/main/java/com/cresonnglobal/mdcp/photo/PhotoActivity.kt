be5d92b58fe552e28dc450d9800b2fa02abc193d
simonojok19 <simonojok19@gmail.com>
2020-08-25 18:53:43 +0300

    added photo activty

---
+++ app/src/main/java/com/cresonnglobal/mdcp/photo/PhotoActivity.kt
@@ -0,0 +1,12 @@
+package com.cresonnglobal.mdcp.photo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cresonnglobal.mdcp.R
+
+class PhotoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo)
+    }
+}
