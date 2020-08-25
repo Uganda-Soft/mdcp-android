37c99d227ec9f954baf737c23c4efe1e5e03c134
simonojok19 <simonojok19@gmail.com>
2020-08-25 18:32:58 +0300

    added decimal activity

---
+++ app/src/main/java/com/cresonnglobal/mdcp/decimal/DecimalActivity.kt
@@ -0,0 +1,12 @@
+package com.cresonnglobal.mdcp.decimal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cresonnglobal.mdcp.R
+
+class DecimalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_decimal)
+    }
+}
