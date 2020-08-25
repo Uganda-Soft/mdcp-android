6b2d594d6a628ad9e0656d2baa403e19b3b33fd0
simonojok19 <simonojok19@gmail.com>
2020-08-25 18:52:10 +0300

    added video activity

---
+++ app/src/main/java/com/cresonnglobal/mdcp/video/VideoActivity.kt
@@ -0,0 +1,12 @@
+package com.cresonnglobal.mdcp.video
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cresonnglobal.mdcp.R
+
+class VideoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video)
+    }
+}
