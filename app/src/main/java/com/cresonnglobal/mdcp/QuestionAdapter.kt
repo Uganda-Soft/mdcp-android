a4a5f49c95be0e4db4f8e3f4b8a9c40b52694398
simonojok19 <simonojok19@gmail.com>
2020-08-25 20:35:17 +0300

    added recyclerview and view holder

---
+++ app/src/main/java/com/cresonnglobal/mdcp/QuestionAdapter.kt
@@ -0,0 +1,10 @@
+package com.cresonnglobal.mdcp
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class QuestionAdapter {
+    class QuestionViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    }
+}
