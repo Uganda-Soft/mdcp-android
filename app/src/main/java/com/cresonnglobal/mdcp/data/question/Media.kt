af72d3567e5eb76ff8f52ebe0c49c3ebc0544cfc
simonojok19 <simonojok19@gmail.com>
2020-08-25 23:25:04 +0300

    implemented parcelable

--- app/src/main/java/com/cresonnglobal/mdcp/data/question/Media.kt
+++ app/src/main/java/com/cresonnglobal/mdcp/data/question/Media.kt
@@ -1,6 +1,38 @@
 package com.cresonnglobal.mdcp.data.question
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class Media (
-    var video: String,
-    var audio: String
-)
+    var video: String?,
+    var audio: String?
+): Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString()
+    ) {
+    }
+
+    override fun toString(): String {
+        return super.toString()
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(video)
+        parcel.writeString(audio)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Media> {
+        override fun createFromParcel(parcel: Parcel): Media {
+            return Media(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Media?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
--- app/src/main/java/com/cresonnglobal/mdcp/data/question/Question.kt
+++ app/src/main/java/com/cresonnglobal/mdcp/data/question/Question.kt
@@ -1,47 +1,101 @@
 package com.cresonnglobal.mdcp.data.question
 
+import android.os.Parcel
 import android.os.Parcelable
 
 data class Question(
-    var type_name: QuestionType,
-    var name: String,
-    var label: String,
-    var hint: String,
-    var default: String,
-    var appearance: String,
-    var constraint: Array<String>,
-    var constraint_message: Array<String>,
-    var relevance: String,
-    var disabled: String,
-    var required: String,
-    var required_message: Array<String>,
-    var read_only: String,
-    var calculation: String,
-    var repeat_count: String,
-    var media: Media,
-    var choice_filter: String,
-    var note: String,
-    var response_note: String,
-    var publishable: String,
-    var minimum_seconds: String
+    var type_name: QuestionType?,
+    var name: String?,
+    var label: String?,
+    var hint: String?,
+    var default: String?,
+    var appearance: String?,
+    var constraint: Array<String>?,
+    var constraint_message: Array<String>?,
+    var relevance: String?,
+    var disabled: String?,
+    var required: String?,
+    var required_message: Array<String>?,
+    var read_only: String?,
+    var calculation: String?,
+    var repeat_count: String?,
+    var media: Media?,
+    var choice_filter: String?,
+    var note: String?,
+    var response_note: String?,
+    var publishable: String?,
+    var minimum_seconds: String?
 ) : Parcelable{
+
+
+    constructor(parcel: Parcel) : this(
+        parcel.readParcelable(QuestionType::class.java.classLoader),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.createStringArray(),
+        parcel.createStringArray(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.createStringArray(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readParcelable(Media::class.java.classLoader),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString()
+    ) {}
+
+
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeParcelable(type_name, flags)
+        parcel.writeString(name)
+        parcel.writeString(label)
+        parcel.writeString(hint)
+        parcel.writeString(default)
+        parcel.writeString(appearance)
+        parcel.writeStringArray(constraint)
+        parcel.writeStringArray(constraint_message)
+        parcel.writeString(relevance)
+        parcel.writeString(disabled)
+        parcel.writeString(required)
+        parcel.writeStringArray(required_message)
+        parcel.writeString(read_only)
+        parcel.writeString(calculation)
+        parcel.writeString(repeat_count)
+        parcel.writeParcelable(media, flags)
+        parcel.writeString(choice_filter)
+        parcel.writeString(note)
+        parcel.writeString(response_note)
+        parcel.writeString(publishable)
+        parcel.writeString(minimum_seconds)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
-        if (other !is Question) return false
+        if (javaClass != other?.javaClass) return false
 
-        if (type_name != other.type_name) return false
+        other as Question
+
         if (name != other.name) return false
         if (label != other.label) return false
         if (hint != other.hint) return false
         if (default != other.default) return false
         if (appearance != other.appearance) return false
-        if (!constraint.contentEquals(other.constraint)) return false
-        if (!constraint_message.contentEquals(other.constraint_message)) return false
         if (relevance != other.relevance) return false
         if (disabled != other.disabled) return false
         if (required != other.required) return false
-        if (!required_message.contentEquals(other.required_message)) return false
         if (read_only != other.read_only) return false
         if (calculation != other.calculation) return false
         if (repeat_count != other.repeat_count) return false
@@ -56,27 +110,33 @@ data class Question(
     }
 
     override fun hashCode(): Int {
-        var result = type_name.hashCode()
-        result = 31 * result + name.hashCode()
-        result = 31 * result + label.hashCode()
-        result = 31 * result + hint.hashCode()
-        result = 31 * result + default.hashCode()
-        result = 31 * result + appearance.hashCode()
-        result = 31 * result + constraint.contentHashCode()
-        result = 31 * result + constraint_message.contentHashCode()
-        result = 31 * result + relevance.hashCode()
-        result = 31 * result + disabled.hashCode()
-        result = 31 * result + required.hashCode()
-        result = 31 * result + required_message.contentHashCode()
-        result = 31 * result + read_only.hashCode()
-        result = 31 * result + calculation.hashCode()
-        result = 31 * result + repeat_count.hashCode()
-        result = 31 * result + media.hashCode()
-        result = 31 * result + choice_filter.hashCode()
-        result = 31 * result + note.hashCode()
-        result = 31 * result + response_note.hashCode()
-        result = 31 * result + publishable.hashCode()
-        result = 31 * result + minimum_seconds.hashCode()
+        var result = name?.hashCode() ?: 0
+        result = 31 * result + (label?.hashCode() ?: 0)
+        result = 31 * result + (hint?.hashCode() ?: 0)
+        result = 31 * result + (default?.hashCode() ?: 0)
+        result = 31 * result + (appearance?.hashCode() ?: 0)
+        result = 31 * result + (relevance?.hashCode() ?: 0)
+        result = 31 * result + (disabled?.hashCode() ?: 0)
+        result = 31 * result + (required?.hashCode() ?: 0)
+        result = 31 * result + (read_only?.hashCode() ?: 0)
+        result = 31 * result + (calculation?.hashCode() ?: 0)
+        result = 31 * result + (repeat_count?.hashCode() ?: 0)
+        result = 31 * result + (media?.hashCode() ?: 0)
+        result = 31 * result + (choice_filter?.hashCode() ?: 0)
+        result = 31 * result + (note?.hashCode() ?: 0)
+        result = 31 * result + (response_note?.hashCode() ?: 0)
+        result = 31 * result + (publishable?.hashCode() ?: 0)
+        result = 31 * result + (minimum_seconds?.hashCode() ?: 0)
         return result
+    }
+
+    companion object CREATOR : Parcelable.Creator<Question> {
+        override fun createFromParcel(parcel: Parcel): Question {
+            return Question(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Question?> {
+            return arrayOfNulls(size)
+        }
     }
 }
