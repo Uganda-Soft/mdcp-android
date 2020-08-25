b6c0da24c27139d86bf76301051ce895285d8e61
simonojok19 <simonojok19@gmail.com>
2020-08-25 20:43:38 +0300

    added the view holder

--- app/src/main/java/com/cresonnglobal/mdcp/QuestionAdapter.kt
+++ app/src/main/java/com/cresonnglobal/mdcp/QuestionAdapter.kt
@@ -3,11 +3,18 @@ package com.cresonnglobal.mdcp
 import android.view.View
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.cresonnglobal.mdcp.data.question.Question
 
 class QuestionAdapter {
     class QuestionViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         private val numberTextView: TextView = itemView.findViewById(R.id.question_number)
         private val labelTextView: TextView = itemView.findViewById(R.id.question_label)
-        private val statusTextView: TextView = itemView.findViewById(R.id.question_status)
+        private val statusTextView: View = itemView.findViewById(R.id.question_status)
+
+        public fun bind(number: Int, question: Question) {
+            numberTextView.text = number.toString()
+            labelTextView.text = question.label
+//            statusTextView.text = "something"
+        }
     }
 }
