e0754a436552ab43af08f8e16afff469268fbfd6
simonojok19 <simonojok19@gmail.com>
2020-08-25 18:37:43 +0300

    added date and time activity

---
+++ app/src/main/java/com/cresonnglobal/mdcp/date/DateActivity.kt
@@ -0,0 +1,12 @@
+package com.cresonnglobal.mdcp.date
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cresonnglobal.mdcp.R
+
+class DateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_date)
+    }
+}
