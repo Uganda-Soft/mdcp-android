acd628d1010c19b15974a378b93c40017f0eef41
simonojok19 <simonojok19@gmail.com>
2020-08-26 14:22:34 +0300

    added text area activity

--- app/src/main/java/com/cresonnglobal/mdcp/textarea/TextAreaActivity.kt
+++ app/src/main/java/com/cresonnglobal/mdcp/textarea/TextAreaActivity.kt
@@ -3,13 +3,20 @@ package com.cresonnglobal.mdcp.textarea
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.cresonnglobal.mdcp.R
+import com.cresonnglobal.mdcp.data.question.Question
+import kotlinx.android.synthetic.main.activity_text_area.*
 
 class TextAreaActivity : AppCompatActivity() {
     companion object {
-        public final val QUESTION = "com.cresonnglobal.mdcp.textarea.TextAreaActivity.QUESTION"
+        public final const val QUESTION = "com.cresonnglobal.mdcp.textarea.TextAreaActivity.QUESTION"
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_text_area)
+
+        val question: Question? = intent.getParcelableExtra<Question>(QUESTION)
+        name.text = question?.name
+        label.text = question?.label
+        hint.text = question?.hint
     }
 }
