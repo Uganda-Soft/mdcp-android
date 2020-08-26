d266ecfaf24bef1698c7b17a48ce458ce1e4f378
simonojok19 <simonojok19@gmail.com>
2020-08-26 12:48:18 +0300

    added boolean activity

--- app/src/main/java/com/cresonnglobal/mdcp/booleans/BooleanActivity.kt
+++ app/src/main/java/com/cresonnglobal/mdcp/booleans/BooleanActivity.kt
@@ -3,6 +3,8 @@ package com.cresonnglobal.mdcp.booleans
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.cresonnglobal.mdcp.R
+import com.cresonnglobal.mdcp.data.question.Question
+import kotlinx.android.synthetic.main.activity_boolean.*
 
 class BooleanActivity : AppCompatActivity() {
     companion object {
@@ -12,5 +14,12 @@ class BooleanActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_boolean)
+
+        val question: Question? = intent.getParcelableExtra<Question>(QUESTION)
+
+// There is some name mismatch here this is not mistake
+        name.text = question?.name
+        label.text = question?.label
+        hint.text = question?.hint
     }
 }
