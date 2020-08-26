195cbfb44bd8a29702c5b677b7edfa45b9ba4c0a
simonojok19 <simonojok19@gmail.com>
2020-08-26 16:00:35 +0300

    added some codes time for testing contraints activity

--- app/src/main/java/com/cresonnglobal/mdcp/helpers/contraints/ConstraintViewActivityActivity.kt
+++ app/src/main/java/com/cresonnglobal/mdcp/helpers/contraints/ConstraintViewActivityActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import com.cresonnglobal.mdcp.R
 
 class ConstraintViewActivityActivity : Activity() {
+    companion object {
+        public final const val CONSTRAINTS = "com.cresonnglobal.mdcp.helpers.contraints.ConstraintViewActivityActivity.CONSTRAINTS"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_constraint_view_activity)
