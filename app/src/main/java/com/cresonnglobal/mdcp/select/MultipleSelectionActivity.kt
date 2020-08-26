8f5627df5a34e353bbe35041d5c94c68d2b7992d
simonojok19 <simonojok19@gmail.com>
2020-08-26 12:51:47 +0300

    added mutiple select activity

---
+++ app/src/main/java/com/cresonnglobal/mdcp/select/MultipleSelectionActivity.kt
@@ -0,0 +1,12 @@
+package com.cresonnglobal.mdcp.select
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cresonnglobal.mdcp.R
+
+class MultipleSelectionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_multiple_selection)
+    }
+}
