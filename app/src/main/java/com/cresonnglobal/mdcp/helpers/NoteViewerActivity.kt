874274c2296dfd21e0aae4639629e40460b5e8b7
simonojok19 <simonojok19@gmail.com>
2020-08-26 17:17:45 +0300

    added the note

--- app/src/main/java/com/cresonnglobal/mdcp/helpers/NoteViewerActivity.kt
+++ app/src/main/java/com/cresonnglobal/mdcp/helpers/NoteViewerActivity.kt
@@ -3,6 +3,7 @@ package com.cresonnglobal.mdcp.helpers
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.cresonnglobal.mdcp.R
+import kotlinx.android.synthetic.main.activity_note_viewer.*
 
 class NoteViewerActivity : AppCompatActivity() {
     companion object {
@@ -11,5 +12,11 @@ class NoteViewerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_viewer)
+
+        val inComingIntent = intent
+        val noteText:String? = inComingIntent.getStringExtra(NOTE_TEXT)
+        noteText?.let {
+            note.text = it
+        }
     }
 }
