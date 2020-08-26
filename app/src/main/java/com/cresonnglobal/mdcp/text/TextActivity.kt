536b2d484ed750d71314fbc3831f8a036bdcb3a2
simonojok19 <simonojok19@gmail.com>
2020-08-26 16:52:50 +0300

    finished text activity

--- app/src/main/java/com/cresonnglobal/mdcp/text/TextActivity.kt
+++ app/src/main/java/com/cresonnglobal/mdcp/text/TextActivity.kt
@@ -1,23 +1,52 @@
 package com.cresonnglobal.mdcp.text
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.cresonnglobal.mdcp.R
 import com.cresonnglobal.mdcp.data.question.Question
+import com.cresonnglobal.mdcp.helpers.contraints.ConstraintViewActivityActivity
 import kotlinx.android.synthetic.main.activity_text.*
 
 class TextActivity : AppCompatActivity() {
     companion object {
         public final const val QUESTION: String = "com.cresonnglobal.mdcp.text.TextActivity.QUESTION"
     }
+    private var question:Question? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_text)
 
-        val question: Question? = intent.getParcelableExtra<Question>(QUESTION)
+        question = intent.getParcelableExtra<Question>(QUESTION)
         name.text = question?.name
         label.text = question?.label
         hint.text = question?.hint
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.screen_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.action_clear -> {
+                // clear answers
+                return true
+            }
+            R.id.action_view_constraints -> {
+                val intent = Intent(this,  ConstraintViewActivityActivity::class.java)
+                intent.putExtra(ConstraintViewActivityActivity.CONSTRAINTS, question?.constraint_message)
+                startActivity(intent)
+            }
+
+            R.id.action_view_help -> {
+                // show help
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
