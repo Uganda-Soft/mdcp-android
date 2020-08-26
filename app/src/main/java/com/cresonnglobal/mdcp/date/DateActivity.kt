b03fc8d096af8e99f0a60250fc07dcf67be990c7
simonojok19 <simonojok19@gmail.com>
2020-08-26 14:25:03 +0300

    added decimal activity

--- app/src/main/java/com/cresonnglobal/mdcp/date/DateActivity.kt
+++ app/src/main/java/com/cresonnglobal/mdcp/date/DateActivity.kt
@@ -3,13 +3,20 @@ package com.cresonnglobal.mdcp.date
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.cresonnglobal.mdcp.R
+import com.cresonnglobal.mdcp.data.question.Question
+import kotlinx.android.synthetic.main.activity_decimal.*
 
 class DateActivity : AppCompatActivity() {
     companion object {
-        public final val QUESTION = "com.cresonnglobal.mdcp.date.DateActivity.QUESTION"
+        public final const val QUESTION = "com.cresonnglobal.mdcp.date.DateActivity.QUESTION"
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_date)
+
+        val question: Question? = intent.getParcelableExtra<Question>(QUESTION)
+        name.text = question?.name
+        label.text = question?.label
+        hint.text = question?.hint
     }
 }
