27078eade0640b273fa7a870bc6dba0724ad03a2
simonojok19 <simonojok19@gmail.com>
2020-08-26 14:24:03 +0300

    added decimal activity

--- app/src/main/java/com/cresonnglobal/mdcp/decimal/DecimalActivity.kt
+++ app/src/main/java/com/cresonnglobal/mdcp/decimal/DecimalActivity.kt
@@ -3,13 +3,20 @@ package com.cresonnglobal.mdcp.decimal
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.cresonnglobal.mdcp.R
+import com.cresonnglobal.mdcp.data.question.Question
+import kotlinx.android.synthetic.main.activity_decimal.*
 
 class DecimalActivity : AppCompatActivity() {
     companion object {
-        public final val QUESTION = "com.cresonnglobal.mdcp.decimal.DecimalActivity.QUESTION"
+        public final const val QUESTION = "com.cresonnglobal.mdcp.decimal.DecimalActivity.QUESTION"
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_decimal)
+
+        val question: Question? = intent.getParcelableExtra<Question>(QUESTION)
+        name.text = question?.name
+        label.text = question?.label
+        hint.text = question?.hint
     }
 }
