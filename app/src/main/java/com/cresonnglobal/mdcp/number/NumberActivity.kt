eb48f5c362a62664327b2726aae17ed1f46ced64
simonojok19 <simonojok19@gmail.com>
2020-08-26 14:21:02 +0300

    fixed number activity

--- app/src/main/java/com/cresonnglobal/mdcp/number/NumberActivity.kt
+++ app/src/main/java/com/cresonnglobal/mdcp/number/NumberActivity.kt
@@ -3,14 +3,21 @@ package com.cresonnglobal.mdcp.number
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.cresonnglobal.mdcp.R
+import com.cresonnglobal.mdcp.data.question.Question
+import kotlinx.android.synthetic.main.activity_number.*
 
 class NumberActivity : AppCompatActivity() {
     companion object {
-        public final val QUESTION: String = "com.cresonnglobal.mdcp.number.NumberActivity.QUESTION"
+        public final const val QUESTION: String = "com.cresonnglobal.mdcp.number.NumberActivity.QUESTION"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_number)
+
+        val question: Question? = intent.getParcelableExtra<Question>(QUESTION)
+        name.text = question?.name
+        label.text = question?.label
+        hint.text = question?.hint
     }
 }
