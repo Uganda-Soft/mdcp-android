89d94c9dd56facdbe081da7e445249805c64c598
simonojok19 <simonojok19@gmail.com>
2020-08-26 15:48:35 +0300

    added helpers activity

---
+++ app/src/main/java/com/cresonnglobal/mdcp/helpers/contraints/ConstraintViewActivityActivity.kt
@@ -0,0 +1,11 @@
+package com.cresonnglobal.mdcp.helps.contraints
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConstraintViewActivityActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint_view_activity)
+    }
+}
---
+++ app/src/main/java/com/cresonnglobal/mdcp/helpers/helper/HelpViewActivity.kt
@@ -0,0 +1,11 @@
+package com.cresonnglobal.mdcp.helpers.helper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HelpViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_help_view)
+    }
+}
