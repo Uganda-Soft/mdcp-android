89746daa31e3979c59221202b9dca24b885f5430
simonojok19 <simonojok19@gmail.com>
2020-08-26 13:17:24 +0300

    added bindings

--- app/src/main/java/com/cresonnglobal/mdcp/select/MultipleSelectionActivity.kt
+++ app/src/main/java/com/cresonnglobal/mdcp/select/MultipleSelectionActivity.kt
@@ -3,10 +3,20 @@ package com.cresonnglobal.mdcp.select
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.cresonnglobal.mdcp.R
+import com.cresonnglobal.mdcp.data.question.Question
+import kotlinx.android.synthetic.main.activity_multiple_selection.*
 
 class MultipleSelectionActivity : AppCompatActivity() {
+    companion object {
+        public final const val QUESTION = "com.cresonnglobal.mdcp.photo.MultipleSelectionActivity.QUESTION"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_multiple_selection)
+
+        val question: Question? = intent.getParcelableExtra<Question>(QUESTION)
+        name.text = question?.name
+        label.text = question?.label
+        hint.text = question?.hint
     }
 }
