6c5022b4a9317b9ed45724f19be6bfd124892433
simonojok19 <simonojok19@gmail.com>
2020-09-08 00:22:24 +0300

    added the first relation

--- app/src/main/java/com/cresonnglobal/mdcp/data/question/QuestionDao.kt
+++ app/src/main/java/com/cresonnglobal/mdcp/data/question/QuestionDao.kt
@@ -1,8 +1,20 @@
 package com.cresonnglobal.mdcp.data.question
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Transaction
 
 @Dao
 interface QuestionDao {
+    @Insert
+    fun insertQuestion(question: Question)
+
+    @Query("SELECT * FROM question")
+    fun queryQuestion(): List<Question>
+
+    @Transaction
+    @Query("SELECT * FROM question")
+    fun queryQuestionType(): List<QuestionType>
 
 }
