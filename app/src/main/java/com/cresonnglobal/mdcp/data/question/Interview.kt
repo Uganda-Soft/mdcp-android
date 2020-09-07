c37c92c29826bb75cf432c73e005eb8567977eaa
simonojok19 <simonojok19@gmail.com>
2020-09-07 22:22:54 +0300

    adding relations

--- app/src/main/java/com/cresonnglobal/mdcp/data/question/Interview.kt
+++ app/src/main/java/com/cresonnglobal/mdcp/data/question/Interview.kt
@@ -1,7 +1,21 @@
 package com.cresonnglobal.mdcp.data.question
 
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.Relation
+
+@Entity(tableName = "interview")
 data class Interview(
-    val meta: Meta?,
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    val meta: Meta,
     val questions: List<Question>
-) {
-}
+)
+
+data class InterviewMeta(
+    @Embedded val interview: Interview,
+    @Relation(
+        parentColumn = "id",
+        entityColumn = "id"
+    ) val meta: Meta
+)
--- app/src/main/java/com/cresonnglobal/mdcp/data/question/Meta.kt
+++ app/src/main/java/com/cresonnglobal/mdcp/data/question/Meta.kt
@@ -3,9 +3,11 @@ package com.cresonnglobal.mdcp.data.question
 import android.os.Parcel
 import android.os.Parcelable
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-@Entity()
+@Entity(tableName = "meta")
 data class Meta(
+    @PrimaryKey(autoGenerate = true) val id: Int,
     var note: String,
     var basic_info: List<BasicInfo>,
     var constraint_message: List<String>
