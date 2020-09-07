1efef0ea2b0d7111223e985ca89031d82894e3fc
simonojok19 <simonojok19@gmail.com>
2020-09-07 23:53:10 +0300

    added constraints

---
+++ app/src/main/java/com/cresonnglobal/mdcp/data/question/ConstraintDao.kt
@@ -0,0 +1,16 @@
+package com.cresonnglobal.mdcp.data.question
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Index
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface ConstraintDao {
+    @Insert
+    fun insertConstraint(constraint: Constraint)
+
+    @Query("SELECT * FROM constraints")
+    fun queryConstraints(): LiveData<List<Constraint>>
+}
