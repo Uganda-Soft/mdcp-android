b8e2c63e34cffeb13229d4301025177e999d5069
simonojok19 <simonojok19@gmail.com>
2020-09-07 23:00:29 +0300

    added the answer entity

---
+++ app/src/main/java/com/cresonnglobal/mdcp/data/question/Answer.kt
@@ -0,0 +1,15 @@
+package com.cresonnglobal.mdcp.data.question
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "answer")
+data class Answer(
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    var message: String,
+    var date: String,
+    var time: String,
+    var duration: String,
+    var logitude: Long,
+    var latitude: Long
+)
