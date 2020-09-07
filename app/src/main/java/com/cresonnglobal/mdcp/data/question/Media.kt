b2f761e165cf0a6385d6f91b779f0a239f432665
simonojok19 <simonojok19@gmail.com>
2020-09-07 22:14:09 +0300

    app faulting having errors

--- app/src/main/java/com/cresonnglobal/mdcp/data/question/Media.kt
+++ app/src/main/java/com/cresonnglobal/mdcp/data/question/Media.kt
@@ -4,35 +4,6 @@ import android.os.Parcel
 import android.os.Parcelable
 
 data class Media (
-    var video: String?,
-    var audio: String?
-): Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString(),
-        parcel.readString()
-    ) {
-    }
-
-    override fun toString(): String {
-        return super.toString()
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(video)
-        parcel.writeString(audio)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Media> {
-        override fun createFromParcel(parcel: Parcel): Media {
-            return Media(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Media?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+    var video: String,
+    var audio: String
+)
