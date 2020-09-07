3af6df60ffc4edfa62a9e51a13d8d7832ebaf268
simonojok19 <simonojok19@gmail.com>
2020-09-08 00:08:31 +0300

    added required message

---
+++ app/src/main/java/com/cresonnglobal/mdcp/data/question/RequiredMessageDao.kt
@@ -0,0 +1,15 @@
+package com.cresonnglobal.mdcp.data.question
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface RequiredMessageDao {
+    @Insert
+    fun insertRequiredMessage(requiredMessage: RequiredMessage)
+
+    @Query("SELECT * FROM require_message")
+    fun queryRequiredMessage(): LiveData<List<RequiredMessage>>
+}
