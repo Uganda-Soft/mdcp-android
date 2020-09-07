f37ad008d0af76c75d39d3212c0c9900cd76f363
simonojok19 <simonojok19@gmail.com>
2020-09-07 22:39:43 +0300

    added constraints message entity

---
+++ app/src/main/java/com/cresonnglobal/mdcp/data/question/ConstraintMessage.kt
@@ -0,0 +1,10 @@
+package com.cresonnglobal.mdcp.data.question
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "constraint_message")
+data class ConstraintMessage(
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    val message: String
+)
