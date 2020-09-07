5f6693283bd98e0f882dad73e8f964bd638070b9
simonojok19 <simonojok19@gmail.com>
2020-09-07 23:19:54 +0300

    added answer dao

--- app/src/main/java/com/cresonnglobal/mdcp/data/CresonnglobalDatabase.kt
+++ app/src/main/java/com/cresonnglobal/mdcp/data/CresonnglobalDatabase.kt
@@ -4,14 +4,14 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.cresonnglobal.mdcp.data.question.Answer
+import com.cresonnglobal.mdcp.data.question.AnswerDao
 
-@Database(entities = arrayOf(), version = 1, exportSchema = false)
+@Database(entities = [
+    Answer::class
+], version = 1, exportSchema = false)
 abstract class CresonnglobalDatabase: RoomDatabase() {
-
-
-
-
-
+    abstract fun answerDao(): AnswerDao
 
     companion object {
         @Volatile
