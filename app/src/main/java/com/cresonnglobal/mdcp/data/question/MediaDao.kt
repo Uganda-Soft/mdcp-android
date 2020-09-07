9a4f776cac2f96c31c010476b0eb7ff042b9af83
simonojok19 <simonojok19@gmail.com>
2020-09-07 23:58:04 +0300

    added the media

---
+++ app/src/main/java/com/cresonnglobal/mdcp/data/question/MediaDao.kt
@@ -0,0 +1,15 @@
+package com.cresonnglobal.mdcp.data.question
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface MediaDao {
+    @Insert
+    fun insertMedia(media: Media)
+
+    @Query("SELECT * FROM media")
+    fun queryMedia(): LiveData<List<Media>>
+}
