9348dda3fc01e6360da52f2f10a71544957f8800
simonojok19 <simonojok19@gmail.com>
2020-09-09 14:49:40 +0300

    added basic info

--- app/src/main/java/com/cresonnglobal/mdcp/data/question/Meta.kt
+++ app/src/main/java/com/cresonnglobal/mdcp/data/question/Meta.kt
@@ -1,13 +1,12 @@
 package com.cresonnglobal.mdcp.data.question
 
 
-import androidx.room.Embedded
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-import androidx.room.Relation
+import androidx.room.*
 
 @Entity(tableName = "meta")
 data class Meta(
     @PrimaryKey(autoGenerate = true) val id: Int,
-    var note: String
+    var note: String,
+    @Ignore
+    var basic_info: List<BasicInfo>?
 )
