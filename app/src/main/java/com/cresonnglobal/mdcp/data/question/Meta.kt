f5a985f07120e6b9fc885562ab84cd3c4a0a2338
simonojok19 <simonojok19@gmail.com>
2020-09-09 14:43:37 +0300

    added started working on the interview

--- app/src/main/java/com/cresonnglobal/mdcp/data/question/Meta.kt
+++ app/src/main/java/com/cresonnglobal/mdcp/data/question/Meta.kt
@@ -11,19 +11,3 @@ data class Meta(
     @PrimaryKey(autoGenerate = true) val id: Int,
     var note: String
 )
-
-data class  MetaBasicInfo(
-    @Embedded val meta: Meta,
-    @Relation(
-        parentColumn = "id",
-        entityColumn = "id"
-    ) val basic_info: List<BasicInfo>
-)
-
-data class MetaConstrainMessage(
-    @Embedded val meta: Meta,
-    @Relation(
-        parentColumn = "id",
-        entityColumn = "id"
-    ) val constraint_message: List<String>
-)
