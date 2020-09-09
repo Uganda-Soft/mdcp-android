dba5827d0969f057e4e3bd1ac1d2a3489455730a
simonojok19 <simonojok19@gmail.com>
2020-09-09 14:02:49 +0300

    added the type to question

--- app/src/main/java/com/cresonnglobal/mdcp/data/question/Question.kt
+++ app/src/main/java/com/cresonnglobal/mdcp/data/question/Question.kt
@@ -9,8 +9,6 @@ import java.util.*
 @Entity(tableName = "question")
 data class Question(
     @PrimaryKey(autoGenerate = true) var id: Int,
-    @NonNull
-    var typeId: Int,
     @Ignore
     var type_name: List<Type>?,
     var name: String,
@@ -37,4 +35,33 @@ data class Question(
     @Ignore
     var media: Media?,
     var answers: List<Answer>?
-)
+) {
+    constructor(
+        var id: Int,
+        var type_name: List<Type>?,
+        var name: String,
+        var label: String,
+        var hint: String,
+        var default: String,
+        var appearance: String,
+        var relevance: String,
+        var disabled: String,
+        var required: String,
+        var read_only: String,
+        var calculation: String,
+        var repeat_count: String,
+        var choice_filter: String,
+        var note: String,
+        var response_note: String,
+        var publishable: String,
+        var minimum_seconds: String,
+        @Ignore
+        var constraint_messages: List<ConstraintMessage>?,
+        @Ignore
+        var constraints: List<Constraint>?,
+        var required_message: List<RequiredMessage>?,
+        @Ignore
+        var media: Media?,
+        var answers: List<Answer>?
+    )
+}
--- app/src/main/java/com/cresonnglobal/mdcp/data/question/Type.kt
+++ app/src/main/java/com/cresonnglobal/mdcp/data/question/Type.kt
@@ -1,11 +1,14 @@
 package com.cresonnglobal.mdcp.data.question
 
+import androidx.annotation.NonNull
 import androidx.room.*
 
 @Entity(tableName = "question_types")
 data class Type(
     @PrimaryKey(autoGenerate = true)
     var id: Int,
+    @NonNull
+    var questionId: Int,
     var type: String,
     var from: String,
     var rangeId: Int
@@ -13,10 +16,11 @@ data class Type(
     @Ignore
     constructor(
         id: Int,
+        questionId: Int,
         options: List<Option>?,
         range: Range?,
         type: String,
         from: String,
         rangeId: Int
-    ) : this(id, type, from, rangeId)
+    ) : this(id, questionId, type, from, rangeId)
 }
