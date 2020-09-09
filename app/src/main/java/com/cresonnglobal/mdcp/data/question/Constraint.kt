ac04cfee534c7e452cb08665c75dcef018b4fdc5
simonojok19 <simonojok19@gmail.com>
2020-09-09 12:54:59 +0300

    binded constraint

--- app/src/main/java/com/cresonnglobal/mdcp/data/question/Constraint.kt
+++ app/src/main/java/com/cresonnglobal/mdcp/data/question/Constraint.kt
@@ -1,10 +1,16 @@
 package com.cresonnglobal.mdcp.data.question
 
+import androidx.annotation.NonNull
 import androidx.room.Entity
+import androidx.room.Ignore
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "constraints")
 data class Constraint(
     @PrimaryKey(autoGenerate = true) val id: Int,
+    @NonNull
+    var questionId: Int,
+    @Ignore
+    var constraints: List<Constraint>?,
     val expression: String
 )
