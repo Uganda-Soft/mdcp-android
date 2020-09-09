d6da20cdc2643b52dfe752bbc217374040c610f7
simonojok19 <simonojok19@gmail.com>
2020-09-09 15:47:16 +0300

    added interview id to meta

--- app/src/main/java/com/cresonnglobal/mdcp/data/question/Meta.kt
+++ app/src/main/java/com/cresonnglobal/mdcp/data/question/Meta.kt
@@ -1,20 +1,24 @@
 package com.cresonnglobal.mdcp.data.question
 
 
+import androidx.annotation.NonNull
 import androidx.room.*
 
 @Entity(tableName = "meta")
 data class Meta(
     @PrimaryKey(autoGenerate = true) var id: Int,
+    @NonNull
+    var interviewId: Int,
     var note: String
 ) {
     @Ignore
     lateinit var basic_info: List<BasicInfo>
     constructor(
         id: Int,
+        interviewId: Int,
         note: String,
         basic_info: List<BasicInfo>
-    ) : this(id, note) {
+    ) : this(id, interviewId, note) {
         this.basic_info = basic_info
     }
 }
