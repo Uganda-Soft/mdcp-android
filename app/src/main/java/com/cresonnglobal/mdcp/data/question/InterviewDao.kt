433179dd13b04457d09d3ea4ba94a399795c426b
simonojok19 <simonojok19@gmail.com>
2020-09-09 15:38:29 +0300

    added the query

--- app/src/main/java/com/cresonnglobal/mdcp/data/question/InterviewDao.kt
+++ app/src/main/java/com/cresonnglobal/mdcp/data/question/InterviewDao.kt
@@ -2,10 +2,13 @@ package com.cresonnglobal.mdcp.data.question
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
+
 @Dao
 interface InterviewDao {
     @Insert
     fun insertInterview(interview: Interview)
 
+    @Query("SELECT * FROM interview ORDER BY id DESC LIMIT 1")
     fun getLatestInterview(): Interview
 }
