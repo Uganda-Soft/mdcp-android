05de382e1e0f47534e179c4b64de05ccc7a260cc
simonojok19 <simonojok19@gmail.com>
2020-09-09 15:58:36 +0300

    meta working well

--- app/src/main/java/com/cresonnglobal/mdcp/data/question/MetaDao.kt
+++ app/src/main/java/com/cresonnglobal/mdcp/data/question/MetaDao.kt
@@ -2,10 +2,13 @@ package com.cresonnglobal.mdcp.data.question
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 
 @Dao
 interface MetaDao {
     @Insert
     fun insertMeta(meta: Meta)
+
+    @Query("SELECT * FROM meta ORDER BY id DESC LIMIT 1")
     fun getLatestMeta(): Meta
 }
