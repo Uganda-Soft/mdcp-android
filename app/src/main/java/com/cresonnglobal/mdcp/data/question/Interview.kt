0755d5928e170d824cad99777de1d93bc7ee2371
simonojok19 <simonojok19@gmail.com>
2020-09-09 14:59:10 +0300

    finished for today

--- app/src/main/java/com/cresonnglobal/mdcp/data/question/Interview.kt
+++ app/src/main/java/com/cresonnglobal/mdcp/data/question/Interview.kt
@@ -6,20 +6,3 @@ import androidx.room.*
 data class Interview(
     @PrimaryKey(autoGenerate = true) val id: Int
 )
-
-data class InterviewMeta(
-    @Embedded val interview: Interview,
-    @Relation(
-        parentColumn = "id",
-        entityColumn = "id"
-    ) val meta: Meta
-)
-
-
-data class InterviewQuestion(
-    @Embedded val interview: Interview,
-    @Relation(
-        parentColumn = "id",
-        entityColumn = "id"
-    ) val Questions: List<Question>
-)
