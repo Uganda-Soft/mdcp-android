72ecd4e3b192e14a7895e4a0e9daccdff616d090
simonojok19 <simonojok19@gmail.com>
2020-09-09 15:11:50 +0300

    adding the interview

--- app/src/main/java/com/cresonnglobal/mdcp/data/question/Interview.kt
+++ app/src/main/java/com/cresonnglobal/mdcp/data/question/Interview.kt
@@ -4,5 +4,27 @@ import androidx.room.*
 
 @Entity(tableName = "interview")
 data class Interview(
-    @PrimaryKey(autoGenerate = true) val id: Int
-)
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    var created: String,
+    var author: String,
+    var company: String,
+    var title: String,
+    var dateline: String
+) {
+    constructor(
+        id: Int,
+        created: String,
+        author: String,
+        company: String,
+        title: String,
+        dateline: String,
+        meta: Meta
+    ) : this(
+        id,
+        created,
+        author,
+        company,
+        title,
+        dateline
+    )
+}
