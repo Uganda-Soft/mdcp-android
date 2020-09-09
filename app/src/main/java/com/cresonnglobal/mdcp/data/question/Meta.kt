09093ed802f0dc698e7f8f71a822664203aae9c6
simonojok19 <simonojok19@gmail.com>
2020-09-09 14:52:50 +0300

    working on basic info

--- app/src/main/java/com/cresonnglobal/mdcp/data/question/Meta.kt
+++ app/src/main/java/com/cresonnglobal/mdcp/data/question/Meta.kt
@@ -5,8 +5,12 @@ import androidx.room.*
 
 @Entity(tableName = "meta")
 data class Meta(
-    @PrimaryKey(autoGenerate = true) val id: Int,
-    var note: String,
-    @Ignore
-    var basic_info: List<BasicInfo>?
-)
+    @PrimaryKey(autoGenerate = true) var id: Int,
+    var note: String
+) {
+    constructor(
+        id: Int,
+        note: String,
+        basic_info: List<BasicInfo>
+    ) : this(id, note)
+}
