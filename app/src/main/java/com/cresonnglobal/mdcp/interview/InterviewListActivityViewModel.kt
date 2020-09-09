fbb358375c2326756963c0098f7acabb7ba606c6
simonojok19 <simonojok19@gmail.com>
2020-09-09 18:09:57 +0300

    added interview activity view model

---
+++ app/src/main/java/com/cresonnglobal/mdcp/interview/InterviewListActivityViewModel.kt
@@ -0,0 +1,8 @@
+package com.cresonnglobal.mdcp.interview
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class InterviewListActivityViewModel (application: Application): AndroidViewModel(application) {
+
+}
