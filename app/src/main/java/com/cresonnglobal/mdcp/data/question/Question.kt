e3770cbddb06d4c54c4af54364c59c881fc76262
simonojok19 <simonojok19@gmail.com>
2020-09-09 14:06:03 +0300

    adding questions

--- app/src/main/java/com/cresonnglobal/mdcp/data/question/Question.kt
+++ app/src/main/java/com/cresonnglobal/mdcp/data/question/Question.kt
@@ -9,8 +9,6 @@ import java.util.*
 @Entity(tableName = "question")
 data class Question(
     @PrimaryKey(autoGenerate = true) var id: Int,
-    @Ignore
-    var type_name: List<Type>?,
     var name: String,
     var label: String,
     var hint: String,
@@ -26,42 +24,31 @@ data class Question(
     var note: String,
     var response_note: String,
     var publishable: String,
-    var minimum_seconds: String,
-    @Ignore
-    var constraint_messages: List<ConstraintMessage>?,
-    @Ignore
-    var constraints: List<Constraint>?,
-    var required_message: List<RequiredMessage>?,
-    @Ignore
-    var media: Media?,
-    var answers: List<Answer>?
+    var minimum_seconds: String
 ) {
     constructor(
-        var id: Int,
-        var type_name: List<Type>?,
-        var name: String,
-        var label: String,
-        var hint: String,
-        var default: String,
-        var appearance: String,
-        var relevance: String,
-        var disabled: String,
-        var required: String,
-        var read_only: String,
-        var calculation: String,
-        var repeat_count: String,
-        var choice_filter: String,
-        var note: String,
-        var response_note: String,
-        var publishable: String,
-        var minimum_seconds: String,
-        @Ignore
-        var constraint_messages: List<ConstraintMessage>?,
-        @Ignore
-        var constraints: List<Constraint>?,
-        var required_message: List<RequiredMessage>?,
-        @Ignore
-        var media: Media?,
-        var answers: List<Answer>?
-    )
+        id: Int,
+        name: String,
+        label: String,
+        hint: String,
+        default: String,
+        appearance: String,
+        relevance: String,
+        disabled: String,
+        required: String,
+        read_only: String,
+        calculation: String,
+        repeat_count: String,
+        choice_filter: String,
+        note: String,
+        response_note: String,
+        publishable: String,
+        minimum_seconds: String,
+        constraint_messages: List<ConstraintMessage>,
+        constraints: List<Constraint>,
+        required_message: List<RequiredMessage>,
+        media: Media,
+        answers: List<Answer>,
+        type_name: List<Type>
+    ) : this()
 }
